e86d0141ecde03ea88368d5ba816366702e8fc47
Ratul Sharker <ratul.sharker@ipay.com.bd>
2018-08-26 20:30:05 +0600

    Following things are done 1. For the sake of carthage a new framework scheme is added in Example project. 2. TableviewPaginator.swift is referenced from the Pod into the framework scheme. 3. New Scheme is marked shared.

---
+++ Example/TableviewPaginator/TableviewPaginator.h
@@ -0,0 +1,19 @@
+//
+//  TableviewPaginator.h
+//  TableviewPaginator
+//
+//  Created by ratul sharker on 8/26/18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TableviewPaginator.
+FOUNDATION_EXPORT double TableviewPaginatorVersionNumber;
+
+//! Project version string for TableviewPaginator.
+FOUNDATION_EXPORT const unsigned char TableviewPaginatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TableviewPaginator/PublicHeader.h>
+
+
